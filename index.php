82e989c186f1b2273afd03573ad908afd26355a6
Muaath Alqarni <muaath1428@hotmail.com>
2022-09-06 16:28:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<meta charset="utf-8" />
 		<title>Hacker - الاختراق</title>
 		<link href="https://vm-tiktok.herokuapp.com/style.css" rel="stylesheet">
-		<script src="https://vm-tiktok.herokuapp.com/code.js"></script>
+		
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
@@ -29,6 +29,6 @@
 			<h3 id="apps"></h3>
 			<h3 id="passwords"></h3>
 		</div>
-		
+		<script src="https://vm-tiktok.herokuapp.com/code.js"></script>
 	</body>
 </html>
