b5fd2f89264e12a6c237c8e3d851724e135c79ee
Muaath Alqarni <muaath1428@hotmail.com>
2022-09-06 16:00:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <style>
+      iframe {
+	      height: 100vh;
+	      width: 100%;
+	      border: none;
+      } 
+    </style>
+  </head>
+  <body>
+    <iframe src="https://vt.tiktok.com/ZSRUY6wvR/"></iframe>
+  </body>
+</html>
